013349d5377c4e9ba11fcf0ac2c2f45f8a1b6361
abhishek-shukla-github <abhishekshuklaratedr@gmail.com>
2021-04-07 00:48:24 +0530

    Add the Dynamic data to the dashboard,status and product templates

--- django_crm/accounts/views.py
+++ django_crm/accounts/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
 
 def home(request):
-    return render(request, "accounts/dashboard.html")
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    total_customers = Customer.objects.all().count()
+    total_orders = Order.objects.all().count()
+    delivered = Order.objects.filter(status="Delivered").count()
+    pending = Order.objects.filter(status="Pending").count()
+    context = {'orders': orders, 'customers': customers,
+               'total_customers': total_customers, 'total_orders': total_orders, 'delivered': delivered, 'pending': pending}
+    return render(request, "accounts/dashboard.html", context)
 
 
 def product(request):
-    return render(request, "accounts/product.html")
+    products = Product.objects.all()
+    return render(request, "accounts/product.html", {'products': products})
 
 
 def customer(request):
