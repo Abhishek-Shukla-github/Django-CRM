2cc7b7c7a188121d83417f091232038dcd40a112
abhishek-shukla-github <abhishekshuklaratedr@gmail.com>
2021-05-04 12:29:14 +0530

    Add Templates and implement their inheritence

--- django_crm/accounts/views.py
+++ django_crm/accounts/views.py
@@ -5,12 +5,12 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Home")
+    return render(request, 'accounts/dashboard.html')
 
 
 def products(request):
-    return HttpResponse("Product")
+    return render(request, 'accounts/products.html')
 
 
 def customer(request):
-    return HttpResponse("Customer")
+    return render(request, 'accounts/customers.html')
